c1de8e04c8bd83affe2bf6a121a7a4e50f7c8227
Camille Wirthlin <camille.wirthlin@gmail.com>
2022-05-20 12:47:08 -0600

    updated code and removed files

--- analyzeLatencyData.py
+++ analyzeLatencyData.py
@@ -4,12 +4,12 @@ from matplotlib import pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 
-csv_files = ['latencyData.csv', 'latencyData1.csv', 'latencyData_shellyrgbw2.csv']
+csv_files = ['latencyData.csv', 'latencyData1.csv', 'latencyData_shellyrgbw2.csv', 'latencyData_ESP32-S2.csv']
 
 for file in csv_files:
     file_name = file.replace('.csv', '')
     latency_df = pd.read_csv(file)
-    latency_df.hist(bins=20, figsize=(20,10))
+    latency_df.hist(bins=40, figsize=(20,10))
     plt.tight_layout()
     plt.savefig(file_name + '_separate.png', dpi=300, bbox_inches='tight')
     latency_df.plot.hist(alpha=0.5, figsize=(20,10))
