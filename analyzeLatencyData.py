cd7531274061319d36b4151afb939967a876c3d1
Camille Wirthlin <camille.wirthlin@gmail.com>
2022-05-23 09:48:34 -0600

    updated analyzeLatencyData.py file for better organization of plots

--- analyzeLatencyData.py
+++ analyzeLatencyData.py
@@ -4,14 +4,22 @@ from matplotlib import pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 
-csv_files = ['desktop_latencies.csv', 'raspberypi_latencies.csv', 'shellyrgbw2_latencies.csv', 'latencyData_ESP32-S2.csv', 'ESP32-S2-AP_latencies.csv']
+csv_files = ['desktop_latencies.csv', 'raspberypi_latencies.csv', 'shellyrgbw2_latencies.csv', 'ESP32-S2-AP_latencies.csv', 'ESP32-S2_STA_1_latencies.csv', 'ESP32-S2_STA_2_latencies.csv']
 
 for file in csv_files:
-    file_name = file.replace('.csv', '')
+    file_name = file.replace('_latencies.csv', '')
     latency_df = pd.read_csv(file)
-    latency_df.hist(bins=40, figsize=(20,10))
-    plt.tight_layout()
-    plt.savefig(file_name + '_separate.png', dpi=300, bbox_inches='tight')
-    latency_df.plot.hist(alpha=0.5, figsize=(20,10))
-    plt.tight_layout()
-    plt.savefig(file_name + '_combined.png', dpi=300, bbox_inches='tight')
+
+    if file.find('ESP') != -1:
+        latency_df.hist(bins=40)
+        plt.tight_layout()
+        plt.savefig(file_name + '.png', dpi=300, bbox_inches='tight')
+    else:
+        latency_df.hist(bins=40)
+        plt.tight_layout()
+        plt.savefig(file_name + '_separate.png', dpi=300, bbox_inches='tight')
+        latency_df.plot.hist(alpha=0.5)
+        plt.tight_layout()
+        plt.savefig(file_name + '_combined.png', dpi=300, bbox_inches='tight')
+
+
