20c500ad1af474ac302162a8aba9f94e2fe73a64
Camille Wirthlin <camille.wirthlin@gmail.com>
2022-05-27 12:52:26 -0600

    Created program to create a server for the ESP32

---
+++ ESP32_WiFi/ESP32_Server/ESP32_Server.ino
@@ -0,0 +1,61 @@
+#include <Uri.h>
+#include <WebServer.h>
+#include <HTTP_Method.h>
+
+#include <WiFi.h>
+
+//net lab ssid and password
+const char* wifi_network_ssid = "NET Lab";
+const char* wifi_network_password = "enterthenetlab";
+
+//ap ssid and password
+const char* soft_ap_ssid = "ESP32 AP";
+const char* soft_ap_password = "password";
+
+//create a webserver
+WebServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  
+  //set up wifi
+  WiFi.softAP(soft_ap_ssid, soft_ap_password);
+  WiFi.begin(wifi_network_ssid, wifi_network_password);
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.println("Connecting to WiFI...");
+  }
+
+  Serial.print("ESP32 IP as soft AP: ");
+  Serial.println(WiFi.softAPIP());
+
+  Serial.print("ESP32 IP on the WiFi network: ");
+  Serial.println(WiFi.localIP());
+
+  //set up server
+  server.on("/", handle_root);
+  server.enableCORS(true);
+
+  server.begin();
+  server.sendHeader(F("Access-Control-Allow-Origin"), F("*"));
+   
+  Serial.println("HTTP server started");
+  Serial.println("CORS is enabled");
+  delay(100);
+}
+
+void loop() {
+  server.handleClient();
+}
+
+String HTML = "<!DOCTYPE html>\
+<html>\
+<body>\
+<h1>Arduino ESP32 Web Server &#9729;</h1>\
+</body>\
+</html>";
+
+void handle_root(){
+  server.send(200, "text/html", HTML);
+}
