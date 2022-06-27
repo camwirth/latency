707a09ba2a6976b4d1c9e7c79cba77ae254a118b
root <root@DESKTOP-16GR9NC.localdomain>
2022-06-27 13:46:45 -0600

    fixed statistic problems

--- checkBridge/check.py
+++ checkBridge/check.py
@@ -1,16 +1,14 @@
 import statistics
 import pandas as pd
 import argparse
-#check using min method
-#check using avg method
-#check using modified avg method
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--stdev', type=int, default=1, help='number of standard deviations threshold is set to')
+parser.add_argument('--stdev', type=int, default=1, help='number of standard deviations threshold is set to')
+parser.add_argument('--size', type=int, default=5, help='size of the data set to analyze')
 args = parser.parse_args()
 
 stdev_num = args.stdev
-STDEV = 0
+size = args.size
 
 def min_method(data, threshold):
     if min(data) < threshold:
@@ -25,20 +23,22 @@ def avg_method(data, threshold):
     else:
         return False
 
+
+
 def find_accuracy(response, LOCAL):
     local = 0
-    bridge = 0
+    remote = 0
     for i in response:
         if i == True:
             local += 1
         else:
-                bridge += 1
+                remote += 1
     if LOCAL is True:
         return local/len(response)
     else:
-        return bridge/len(response)
+        return remote/len(response)
 
-def run(file_path, local, local_stdev):
+def run(file_path, local, threshold):
     min_response = []
     avg_response = []
     sample_data = []
@@ -52,20 +52,12 @@ def run(file_path, local, local_stdev):
         data_list.append(i[0])
 
     if local:
-        local_stdev = statistics.pstdev(data_list)
+        threshold = statistics.mean(data_list) + statistics.pstdev(data_list) * stdev_num
         done = False
-    
-    threshold = local_stdev * stdev_num
-
-    print("the average latency accross all data is " + str(statistics.mean(data_list)))
-    print("the standard deviation is: " + str(statistics.pstdev(data_list)))
-    print("the threshold is: " + str(threshold))
-    print("the minimum latency is: " + str(min(data_list)))
-    print("the maximum latency is: " + str(max(data_list)))
 
     for i in range(len(data_list)):
         sample_data.append(data_list[i])
-        if i % 5 == 0:
+        if i % size == 0:
             j += 1
             min_response.append(min_method(sample_data, threshold))
             avg_response.append(avg_method(sample_data, threshold))
@@ -74,16 +66,23 @@ def run(file_path, local, local_stdev):
     min_accuracy = find_accuracy(min_response, local)
     avg_accuracy = find_accuracy(avg_response, local)
 
+    
+    print("the average latency accross all data is " + str(statistics.mean(data_list)))
+    print("the standard deviation is: " + str(statistics.pstdev(data_list)))
+    print("the threshold is: " + str(threshold))
+    print("the minimum latency is: " + str(min(data_list)))
+    print("the maximum latency is: " + str(max(data_list)))
     print("Using the min method the accuracy is: " + str(min_accuracy*100) + "%")
     print("Using the avg method the accuracy is: " + str(avg_accuracy*100) + "%")
 
     if not done:
         print("\nNGROK")
-        run('~/latency/javascript_latency/data/raspberrypi/ESP32_rp_bridge_netlab_2hrs.csv', False, local_stdev)
+        run('~/latency/javascript_latency/data/raspberrypi/ESP32_rp_bridge_netlab_2hrs.csv', False, threshold)
         print("\nVPN")
-        run('/home/camillewirthlin/latency/javascript_latency/data/raspberrypi/ESP32_rp_VPN_netlab-home.csv', False, local_stdev)
+        run('~/latency/javascript_latency/data/raspberrypi/ESP32_rp_VPN_netlab-home.csv', False, threshold)
         print("\nPORT FORWARD")
-        run('/home/camillewirthlin/latency/javascript_latency/data/raspberrypi/ESP32_rp_portForward_netlab-home.csv', False, local_stdev)
+        run('~/latency/javascript_latency/data/raspberrypi/ESP32_rp_portForward_netlab-home.csv', False, threshold)
+        print("\n")
 
 print("\nLOCAL")
 run('~/latency/javascript_latency/data/ESP32_AP_connection.csv', True, 0)
