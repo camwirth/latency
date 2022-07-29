3549dc3ce531fd8731c92e9ad0b5b77dfce60998
Camille Wirthlin <camille.wirthlin@gmail.com>
2022-07-29 15:42:57 -0600

    created new programs to check the latency with error - prints error code on console

---
+++ check/check_javascript/try.js
@@ -0,0 +1,63 @@
+var run 
+var URLtobepinged = "http://76.27.100.138/piapp/"
+threshold = 22
+
+function run() {
+    run = true;
+    begin_time = new Date().getTime()
+    runtime = 10000 + begin_time
+    get_latency()
+}
+
+function get_latency() {
+    async function makeRequest() {
+        try {
+            console.log('making request')
+
+            current_time = new Date().getTime()
+            if(run==true) {
+                if(current_time >= runtime) {
+                    stop(false)
+                } else {
+                    begin = new Date().getTime()
+                    const response = await axios.get(URLtobepinged)
+                    end = new Date().getTime()
+                    
+                    latency = end - begin
+
+                    if (latency <= threshold) {
+                        console.log('local')
+                        stop(true)
+                    }
+                }
+            }
+        } catch (err) {
+            setTimeout(() => {
+                get_latency()
+            }, 500)
+            end = new Date().getTime()
+            console.log('error')
+            latency = end - begin
+            console.log(latency)
+            if(latency <= threshold) {
+                console.log('local')
+                stop(true)
+            }
+        console.log(err.response.status)    //prints html error status code
+        console.log(err.message)        
+        console.log(err.response.headers)
+        console.log(err.response.data)
+        }
+    }
+
+    makeRequest()
+}
+
+function stop(LOCAL) {
+    run = false
+    if(LOCAL) {
+        document.getElementById('local').innerHTML = 'This device is local'
+    } else {
+        document.getElementById('local').innerHTML = 'This device is not local'
+    }
+}
