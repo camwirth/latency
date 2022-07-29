e341e7c6a81384196db9489c128d8a68ab52685e
Camille Wirthlin <camille.wirthlin@gmail.com>
2022-07-29 12:06:28 -0600

    adjusting js check for errors

--- check/check_javascript/if_local_time.js
+++ check/check_javascript/if_local_time.js
@@ -7,55 +7,157 @@ const myHeaders = new Headers({
 var options = {
     method: 'GET',
     cache: 'no-cache',
-    mode: 'no-cors',
+    mode: 'cors',
     headers: myHeaders
 }
 
-function load_image(LOCAL) {
+function stop(LOCAL) {
     if(LOCAL) {
         document.getElementById('local').innerHTML = 'This device is Local';
     } else {
         document.getElementById('local').innerHTML = 'This device is not Local';
     }
+    console.log('done!')
 }
 
-function check_local() {
+function run() {
+    start = new Date().getTime()
+    endTime = start + 10000
     document.getElementById('local').innerHTML = '';
 
-    start = new Date().getTime();
-    endTime = start + 10000;
-    LOCAL = false;
+    check_local(threshold + 1, endTime)
+}
 
-    getPing().catch(error => {
-        console.log('error!');
-    })
-
-    async function getPing() {
-        now = new Date().getTime();
-
-        if(now < endTime && !LOCAL) {
-            begin = new Date().getTime();
-            await fetch(URLtobepinged, options);
-            end = new Date().getTime();
-
-            latency = end - begin;
-
-            if(latency <= threshold) {
-                LOCAL = true;
-                console.log('reached!')
+function check_local(latency, endTime) {
+    begin = new Date().getTime()
+    if(latency <= threshold) {
+        stop(true)
+    } else if (begin >= endTime) {
+        stop(false)
+    } else {
+        // begin = new Date().getTime()
+        getPing().catch(error => {
+            setTimeout(() => {
+                end = new Date().getTime()
+                latency = end - begin
+                check_local(latency, endTime)
+            }, 500)
+        })
+        async function getPing() {
+            console.log('started')
+            if(begin >= endTime) {
+                console.log('done!')
+                LOCAL = false
+                stop(LOCAL)
             } else {
-                setTimeout(() => {
-                    console.log('failed to reach')
-                    getPing();
-                }, 500)
-            }
-        } else {
-
-            if(LOCAL) {
-                document.getElementById('local').innerHTML = 'This device is Local';
-            } else {
-                document.getElementById('local').innerHTML = 'This device is not Local';
+                console.log('fetching')
+                await fetch(URLtobepinged, options);
             }
         }
     }
-}
+
+
+
+
+
+
+
+
+
+
+    // if(latency <= threshold) {
+    //     stop(true)
+    // }
+    // LOCAL = false
+    // document.getElementById('local').innerHTML = '';
+
+    // start = new Date().getTime();
+    // endTime = start + 10000;
+
+    // begin = new Date().getTime();
+    // getPing().catch(error => {
+    //     setTimeout(() => {
+    //         end = new Date().getTime()
+    //         latency = end - begin
+    //         console.log('continuing')
+    //         check_local(latency)
+    //     }, 500)
+    //     })
+    //     // LOCAL = false
+    //     // console.log('error!');
+    //     // console.log(error)
+    //     // end = new Date().getTime();
+    //     // latency = end - begin
+    //     // console.log(latency)
+    //     // if (end >= endTime ) {
+    //     //     console.log('not done yet')
+    //     // } else {
+    //     //     console.log('done!')
+    //     // }
+
+    //     // if (latency <= threshold) {
+    //     //     LOCAL = true;
+    //     //     stop(LOCAL)
+    //     // } else if(end >= endTime) {
+    //     //     console.log('is it working?')
+    //     //     stop(LOCAL)
+
+    //     // } else {
+    //     //     setTimeout(() => {
+    //     //         console.log('failed to reach')
+    //     //         getPing();
+    //     //     }, 500) 
+    //     // }
+    // })
+
+    // // begin = new Date().getTime();
+    // async function getPing() {
+    //     console.log('started')
+    //     if(begin >= endTime) {
+    //         console.log('done!')
+    //         LOCAL = false
+    //         stop(LOCAL)
+    //     } else {
+    //         console.log('fetching')
+    //         await fetch(URLtobepinged, options);
+    //     }
+
+
+
+
+
+
+        // now = new Date().getTime();
+
+        // if(now < endTime && !LOCAL) {
+        //     begin = new Date().getTime();
+        //     await fetch(URLtobepinged, options);
+        //     end = new Date().getTime();
+
+        //     latency = end - begin;
+
+        //     if(latency <= threshold) {
+        //         LOCAL = true;
+        //         console.log('reached!')
+        //     } else {
+        //         setTimeout(() => {
+        //             console.log('failed to reach')
+        //             getPing();
+        //         }, 500)
+        //     }
+        // } //else {
+        //     if(LOCAL) {
+        //         document.getElementById('local').innerHTML = 'This device is Local';
+        //     } else {
+        //         document.getElementById('local').innerHTML = 'This device is not Local';
+        //     }
+        // }
+    }
+
+    // if(LOCAL) {
+    //     document.getElementById('local').innerHTML = 'This device is Local';
+    // } else {
+    //     document.getElementById('local').innerHTML = 'This device is not Local';
+    // }
+
+// }
